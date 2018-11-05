18258c99ff4b980f8430882ffdf5c717d77f3635
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-11-05 10:55:07 +0000

    models file creted

---
+++ client/src/models/info.js
@@ -0,0 +1,2 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
---
+++ client/src/views/map_info_view.js
@@ -0,0 +1,23 @@
+cost PubSub = require('../helpers/pub_sub.js');
+
+const MapInfoView = function (data) {
+  this.data = data;
+}
+
+MapInfoView.prototype.bindEvents = function () {
+  
+};
+
+MapInfoView.prototype.render = function () {
+
+};
+
+MapInfoView.prototype.createMap = function () {
+
+};
+
+// MapInfoView.prototype.createText = function () {
+//
+// };
+
+module.exports = MapInfoView;

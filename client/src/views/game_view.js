af2eaa7d71e7be6944dc06cf9220d2f35c9cc612
Katie Jeffree <k.jeffree@gmail.com>
2018-11-05 12:05:11 +0000

    created answer divs

--- client/src/views/game_view.js
+++ client/src/views/game_view.js
@@ -18,19 +18,33 @@ GameView.prototype.render = function (questionAndAnswer) {
 
 
 
-
   const answer1Container = document.createElement("div");
   answer1Container.id = "answer1";
+  answer1Container.value = questionAndAnswer.qAndA.answer1.status;
+  answer1Container.textContent = questionAndAnswer.qAndA.answer1.content;
 
   const answer2Container = document.createElement("div");
   answer2Container.id = "answer2";
+  answer2Container.value = questionAndAnswer.qAndA.answer2.status;
+  answer2Container.textContent = questionAndAnswer.qAndA.answer2.content;
 
   const answer3Container = document.createElement("div");
   answer3Container.id = "answer3";
+  answer3Container.value = questionAndAnswer.qAndA.answer3.status;
+  answer3Container.textContent = questionAndAnswer.qAndA.answer3.content;
 
   const answer4Container = document.createElement("div");
   answer4Container.id = "answer4";
+  answer4Container.value = questionAndAnswer.qAndA.answer4.status;
+  answer4Container.textContent = questionAndAnswer.qAndA.answer4.content;
+  console.log(answer4Container);
 
+  answersContainer.appendChild(answer1Container)
+  answersContainer.appendChild(answer2Container)
+  answersContainer.appendChild(answer3Container)
+  answersContainer.appendChild(answer4Container)
+
+  this.container.appendChild(answersContainer)
 
 
 };

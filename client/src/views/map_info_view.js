5ec273a1fed8b5b9cc5c150d2265fc203bc59bd2
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-11-05 13:42:35 +0000

    map start point reset

--- client/src/views/map_info_view.js
+++ client/src/views/map_info_view.js
@@ -20,19 +20,16 @@ MapInfoView.prototype.render = function (data) {
 MapInfoView.prototype.createMap = function () {
   const mapContainer = document.createElement('div');
   mapContainer.id = 'mapContainer';
-  var mymap = L.map(mapContainer).setView([51.505, -0.09], 13);
   this.container.appendChild(mapContainer);
-  // this.mapContainer.appendChild(mymap);
-
-
   const CARTOUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png';
   const CARTOTileLayer = new leaflet.TileLayer(CARTOUrl, {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://carto.com/attributions">CARTO</a>'
-});
+  });
 
-this.leafletMap = leaflet.map(this.container)
-  .addLayer(CARTOTileLayer)
-  .setView([51.505, -0.09], 13);
+  this.leafletMap = leaflet.map(this.container)
+    .addLayer(CARTOTileLayer)
+    .setView([37.9838, 23.7275], 4);
+
 };
 
 // MapInfoView.prototype.createText = function () {

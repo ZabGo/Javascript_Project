b4ecb194f99d70cd9bb28a452c4f584a80cf2444
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-11-05 10:59:14 +0000

    model info bind events created

--- client/src/models/info.js
+++ client/src/models/info.js
@@ -1,2 +1,12 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
+
+const Info = function (url) {
+  this.url = url;
+  this.request = new RequestHelper(this.url);
+};
+
+Info.prototype.bindEvents = function () {
+  const data = getData();
+  PubSub.publish('Info:mapInfoData', data);
+};

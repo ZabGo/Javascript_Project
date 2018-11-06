e30ff3e99391a3338ebac2ff44c57ea6858c9c76
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-11-06 13:18:50 +0000

    nav bar added

--- client/src/app.js
+++ client/src/app.js
@@ -3,16 +3,17 @@ const Info = require('./models/info.js');
 const InfoView = require('./views/info_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const displayTag = document.querySelector('div#display');
-  const mapInfoView = new MapInfoView(displayTag);
-  const url = 'http://localhost:3000/wonders/info';
-  const info = new Info(url);
-  info.getData();
-  info.bindEvents();
+    const displayTag = document.querySelector('div#display');
+    const mapInfoView = new MapInfoView(displayTag);
 
-  const infoView = new InfoView(displayTag);
-  infoView.bindEvents();
-  mapInfoView.bindEvents(info);
+  document.querySelector('#info').addEventListener('click', (e) => {
+          const url = 'http://localhost:3000/wonders/info';
+          const info = new Info(url);
+          info.getData();
+          info.bindEvents();
 
-
+          const infoView = new InfoView(displayTag);
+          infoView.bindEvents();
+          mapInfoView.bindEvents(info);
+  })
 })
--- client/src/views/map_info_view.js
+++ client/src/views/map_info_view.js
@@ -8,21 +8,23 @@ const MapInfoView = function (container) {
 MapInfoView.prototype.bindEvents = function () {
   PubSub.subscribe('Info:mapInfoData', (evt) => {
     this.plotPoints(evt.detail);
-});
-this.render();
+  });
+  this.render();
 }
 
 MapInfoView.prototype.render = function (data) {
   this.container.innerHTML = '';
-  const mapContainer = this.createMap();
+  this.createContainer();
+  this.createMap();
 };
 
-
-
-MapInfoView.prototype.createMap = function () {
+MapInfoView.prototype.createContainer = function () {
   const mapContainer = document.createElement('div');
   mapContainer.id = 'mapContainer';
   this.container.appendChild(mapContainer);
+};
+
+MapInfoView.prototype.createMap = function () {
   const CARTOUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png';
   const CARTOTileLayer = new leaflet.TileLayer(CARTOUrl, {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://carto.com/attributions">CARTO</a>'
@@ -40,7 +42,6 @@ MapInfoView.prototype.plotPoints = function (data) {
     var marker = leaflet.marker([longitude, latitude]).addTo(this.leafletMap);
     marker.on('click', (e) => {
       PubSub.publish('MapInfoView:monumentSelected', wonder.name);
-
     })
   })
 };

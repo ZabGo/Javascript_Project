1b1148bf735e9e3d61456316b7b20cedf1e71024
Katie Jeffree <k.jeffree@gmail.com>
2018-11-06 14:25:24 +0000

    map now loads after second click

--- client/src/views/map_info_view.js
+++ client/src/views/map_info_view.js
@@ -8,8 +8,10 @@ const MapInfoView = function (container) {
 MapInfoView.prototype.bindEvents = function () {
   PubSub.subscribe('Info:mapInfoData', (evt) => {
     this.plotPoints(evt.detail);
-  });
-  this.render();
+
+
+});
+this.render();
 }
 
 MapInfoView.prototype.render = function (data) {
@@ -29,7 +31,9 @@ MapInfoView.prototype.createMap = function () {
   const CARTOTileLayer = new leaflet.TileLayer(CARTOUrl, {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://carto.com/attributions">CARTO</a>'
   });
-
+  if (this.leafletMap != null) {
+    this.leafletMap.remove();
+  }
   this.leafletMap = leaflet.map(this.container)
     .addLayer(CARTOTileLayer)
     .setView([37.9838, 23.7275], 4);
